a8d77879d154c2bd58bbae2eeb9c4d34ac288a8a
Ryan Dawkes <ryandawkes@outlook.com>
2020-12-27 20:00:51 +0000

    user databases setup

--- SmartCycle/SmartCycle/App.xaml.cs
+++ SmartCycle/SmartCycle/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using SmartCycle.Views;
+﻿using SmartCycle.Data;
+using SmartCycle.Views;
 using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -7,6 +8,10 @@ namespace SmartCycle
 {
     public partial class App : Application
     {
+
+        static TokenDatabaseController tokenDatabase;
+        static UserDatabaseController userDatabase;
+
         public App()
         {
             InitializeComponent();
@@ -24,6 +29,32 @@ namespace SmartCycle
 
         protected override void OnResume()
         {
+        }
+
+        // Create User DB 
+        public static UserDatabaseController UserDatabase
+        {
+            get
+            {
+                if(userDatabase == null)
+                {
+                    userDatabase = new UserDatabaseController();
+                }
+                return userDatabase;
+            }
+        }
+
+        // Create Token DB
+        public static TokenDatabaseController TokenDatabase
+        {
+            get
+            {
+                if (tokenDatabase == null)
+                {
+                    tokenDatabase = new TokenDatabaseController();
+                }
+                return tokenDatabase;
+            }
         }
     }
 }
